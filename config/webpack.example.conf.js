50bf5b34ff4549c62acd73174024f32b9ce6f6d2
rynxiao <yuzhongzi91@sina.com>
2020-09-16 20:58:12 +0800

    feat: fix lint

--- config/webpack.example.conf.js
+++ config/webpack.example.conf.js
@@ -1,7 +1,6 @@
-const merge = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
 
 const root = path.resolve(__dirname, '..');
 const examplePath = path.resolve(root, 'examples');
